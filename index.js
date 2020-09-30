4be8ba2ae7567d6b045ea92fb64db774891b0ffa
sfcarz <chrisarojasz2003@gmail.com>
2020-09-30 13:56:09 -0700

    API routes established.

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
-const router = express.Router();
+const PORT = process.env.PORT || 3000;
 
-console.log(router);
+app.get('/', function (req, res) {
+    // console.log(req);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
 
-// const server = process.env.PORT || 3001;
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+});
+
+app.get('/api/notes', function (req, res) {
+    const rawData = fs.readFileSync('model/db.json');
+    res.send(JSON.parse(rawData));
+});
+
+app.post('/api/notes', function (req, res) {
+    const requestData = req.data;
+    console.log(requestData);
+    const rawData = fs.readFileSync('model/db.json');
+    const jsonData = JSON.parse(rawData);
+    console.log(rawData, jsonData);
+});
+
+// app.delete('/api/notes/:id', function (req, res) {
+    
+// })
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log('Im listening');
+})
