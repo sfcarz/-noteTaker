497bb52869abff3aaf107cbac3074aa599c94844
sfcarz <chrisarojasz2003@gmail.com>
2020-10-05 10:00:45 -0700

    added express.static to render my page properly

--- index.js
+++ index.js
@@ -4,36 +4,44 @@ const fs = require("fs");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
+// ***** HTML *****
 app.get('/', function (_req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/notes", function (_req, res) {
-    console.log('route to note working');
+    // console.log('route to note working');
     res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-// ****************************************
+// ****** API *******
 
 app.get("/api/notes", (req, res) => {
     fs.readFile(path.join(__dirname, './db/db.json'), (err, notes) => {
         if (err) throw err;
         res.json(JSON.parse(notes));
-        console.log('api notes is working');
+        // console.log('api notes is working');
     })
 });
 
-app.post("/api/notes", function (req, res) {
+app.post('/api/notes', (req, res) => {
     const requestData = req.body;
-    console.log(requestData);
-    const rawData = fs.readFileSync(path.join(__dirname, "./db/db.json"));
-    console.log(rawData);
-    res.json({});
-    // fs.writeFileSync(path.join(__dirname, './db/db.json'));
-    // const jsonData = JSON.parse(rawData);
-    // console.log(jsonData);
+    const rawData = JSON.parse(fs.readFileSync(path.join(__dirname, "./db/db.json")));
+
+    rawData.push(requestData);
+
+    const strings = JSON.stringify(rawData);
+
+    fs.writeFile('./db/db.json', strings, (err) => {
+        if (err) throw err;
+    });
+
+    res.send(strings);
 });
+
 
 // app.delete('/api/notes/', + 'id', function (req, res) {
 
